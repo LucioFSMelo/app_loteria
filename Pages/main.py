400b2cff4ec9a7874ff8fd5bc6a921467e9b93a9
LuciofsMelo <lucius.mat@gmail.com>
2023-12-21 12:46:09 -0300

    Atualizando o conteúdo do main.

---
+++ Pages/main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.title("Mega Sena - Loteria")
+st.markdown("""Este aplicativo não é uma garantia de que você ganhará no jogo.  
+            Com este aviso eu me insento de qualquer eventual perda sua física ou financeira.  
+            Este aplicativo é uma forma divertida e simplificada para os amantes de jogos,  
+            se você não sabe quais dezenas jogar, este gerador de dezenas é uma boa escolha.  
+            """)
+
